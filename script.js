4c9b8039d7137e6717f9501d3d3ebdf70098b8bb
SiFrancis <103404541+SiFrancis@users.noreply.github.com>
2023-04-19 19:54:45 +0800

    eeeeeee

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
-// Get the container element
+//code for toggling active classes on buttons; taken from W3Schools
 var btnContainer = document.getElementById("btn-container");
 
-// Get all buttons with class="btn" inside the container
+
 var btns = btnContainer.getElementsByClassName("topic-btn");
 
-// Loop through the buttons and add the active class to the current/clicked button
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
     var current = document.getElementsByClassName("active");
     current[0].className = current[0].className.replace(" active", "");
     this.className += " active";
   });
-} 
+}
 
+var main_desc = document.getElementById("main-desc");
+var main_text = document.getElementById("main-text");
+
+main_desc.innerHTML = document.getElementById("intro-desc").innerHTML;
+main_text = document.getElementById("intro").innerHTML;
 function showTopic(name) {
-    document.getElementById("main-desc").innerHTML = document.getElementById(name + "-desc").innerHTML;
+    main_desc.innerHTML = document.getElementById(name + "-desc").innerHTML;
     document.getElementById("main-text").innerHTML = document.getElementById(name).innerHTML;
 }
